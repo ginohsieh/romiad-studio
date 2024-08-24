ecfa4e4d2ef140732e8b80ca68a0b92657184962
gino-hsieh <gino_hsieh@trendmicro.com>
2024-08-24 15:38:56 +0800

    feat: add duration and delay to title

--- romiad-studio-ui/src/app/Main.jsx
+++ romiad-studio-ui/src/app/Main.jsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Text } from '@chakra-ui/react';
+import { Box, Fade, Flex, Text } from '@chakra-ui/react';
 import metadata from "@/config/metadata";
 
 export default function Main() {
@@ -27,16 +27,21 @@ export default function Main() {
         <source data-src="./videos/bg.mp4" type="video/mp4" />
       </Box>
       <Flex w="100%" h="100%" justifyContent="center" direction="column" rowGap="1rem" bgColor="blackAlpha.300">
-        <Text
-          as="h1"
-          align="center"
-          textStyle="website-title"
-        >
-          {metadata.title}
-        </Text>
-        <Flex direction="column" justifyContent="center" rowGap="0.25rem">
-          {keywordLines.map((line, index) => (<Text align="center" key={`keyword-line-${index}`}>{line}</Text>))}
-        </Flex>
+        <Fade in transition={{ enter: { delay: 0.5, duration: 1 } }}>
+          <Text
+            as="h1"
+            align="center"
+            textStyle="website-title"
+          >
+            {metadata.title}
+          </Text>
+
+        </Fade>
+        <Fade in transition={{ enter: { delay: 0.5, duration: 1 } }}>
+          <Flex direction="column" justifyContent="center" rowGap="0.25rem">
+            {keywordLines.map((line, index) => (<Text align="center" key={`keyword-line-${index}`}>{line}</Text>))}
+          </Flex>
+        </Fade>
       </Flex>
     </Flex>
   )
