442e08c2270268410410d66f440120fae3deac14
gino-hsieh <gino_hsieh@trendmicro.com>
2025-02-09 11:48:28 +0800

    feat: update about images

--- romiad-studio-ui/src/app/us/page.js
+++ romiad-studio-ui/src/app/us/page.js
@@ -36,28 +36,6 @@ export default function Us() {
     <main>
       <Flex bgColor="black"><Navigation /></Flex>
       <Flex width="100%" pt="120px" px="4rem" direction="column" alignItems="center" rowGap="4rem">
-        {/* <Flex
-          width="100%"
-          maxWidth="1680px"
-          {...aboutUsProps}
-        >
-          <Flex flex="1" direction="column" justifyContent="center" rowGap="2rem" px="1rem">
-            <Heading {...aboutUsTitleProps}>About us 關於盧米</Heading>
-            <Flex direction="column" justifyContent="center" rowGap="1rem">
-              <Text>「影像的品質，我們嚴謹挑剔；影像的價值，我們律已專一。」</Text>
-              <Text>盧米，是原住民詞語Romiad的中文轉譯，有「日z子；天」的意思，但沿用到「盧米工作室」一名稱上，則衍伸出「挑戰、創新」的涵義。</Text>
-              <Text>透過挑戰視聽感官的影像創作，融合在地與世界潮流影像的特色，在影像領域中創予獨殊之風格，從而建立一套，能以不同角度觀看、敘述影像的方式，使大眾能體會並培養出看待影像更多元的觀點。</Text>
-            </Flex>
-          </Flex>
-          <Flex flex="2">
-            <Image
-              w="100%"
-              objectFit="cover"
-              src="./imgs/about_us.webp"
-              alt="about_us"
-            />
-          </Flex>
-        </Flex> */}
         <AboutSection />
         <ProcessSection />
         <AwardSection />
--- romiad-studio-ui/src/hooks/useAboutImages.js
+++ romiad-studio-ui/src/hooks/useAboutImages.js
@@ -2,16 +2,16 @@ import { useMemo } from 'react'
 
 export default function useAboutImages() {
   const images = useMemo(() => [
-    { src: "./imgs/aboutus/1.png" },
+    { src: "./imgs/aboutus/1.jpg" },
     { src: "./imgs/aboutus/2.jpg" },
     { src: "./imgs/aboutus/3.jpg" },
     { src: "./imgs/aboutus/4.jpg" },
     { src: "./imgs/aboutus/5.jpg" },
-    { src: "./imgs/aboutus/6.png" },
-    { src: "./imgs/aboutus/7.png" },
-    { src: "./imgs/aboutus/8.png" },
-    { src: "./imgs/aboutus/9.png" },
-    { src: "./imgs/aboutus/10.png" },
+    { src: "./imgs/aboutus/6.jpg" },
+    { src: "./imgs/aboutus/7.jpg" },
+    { src: "./imgs/aboutus/8.jpg" },
+    { src: "./imgs/aboutus/9.jpg" },
+    { src: "./imgs/aboutus/10.jpg" },
   ], [])
   return images
 }
